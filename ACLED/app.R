fdbd240be09f0af2f697d07364549b5eba2d4fb3
Teruteru64 <a.sasaki@mpp.hertie-school.org>
2017-12-15 02:08:15 +0100

    update

--- ACLED/app.R
+++ ACLED/app.R
@@ -21,24 +21,24 @@ ui <- fluidPage(
 
 
 server <- function(input, output) {
+
+  ACLED$count <- as.numeric(ACLED$count)
+  
+  ACLED.new <- reactive({
+    filter(ACLED, EVENT_TYPE==input$EVENT_TYPE, country==input$country)
+  })
   
   # make line charts of events
   output$lineChart <- renderPlot({
-    chartData <- switch(input$ACLED,
-                        "World" = list(dat$gr_w,dat$re_w),
-                        "All countries except China" = list(dat$gr_noChina,dat$re_noChina),
-                        "Advanced Economies only" = list(dat$gr_advanced,dat$re_advanced),
-                        "Eurozone" = list(dat$gr_euro,dat$re_euro) 
-    )  
-    ACLED$count <- as.numeric(ACLED$count)
-    g<-ggplot(data=ACLED,aes(x=year, y=count, group=country))
-    g<-g+geom_line() + 
-      geom_point() +
-      ggtitle(paste("Armed Conflicts")) +
-      xlab("Year") + ylab("The number of reported cases")
-    g
-  })
+  g<-ggplot(data=ACLED.new(), aes(x=year, y=count, group=country))
+  g<-g+geom_line(color="steelblue") + 
+    geom_point() +
+    xlab("Year") + ylab("The number of reported cases")
+  g
   
+})
+
 }
+
 
 shinyApp(ui = ui, server = server)
